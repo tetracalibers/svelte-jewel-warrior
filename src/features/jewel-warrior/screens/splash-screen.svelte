e4531404c721783fd371f952ef71eaf420fb7732
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-12-27 23:02:38 +0900

    スプラッシュ画面のスタイリング

--- src/features/jewel-warrior/screens/splash-screen.svelte
+++ src/features/jewel-warrior/screens/splash-screen.svelte
@@ -4,4 +4,35 @@
   export let active = false
 </script>
 
-<Screen {active} />
+<Screen {active}>
+  <section class="screen-content">
+    <h1 class="logo">Jewel <br />Warrior</h1>
+    <button type="button" class="continue-button">Click to continue</button>
+  </section>
+</Screen>
+
+<style>
+  .screen-content {
+    text-align: center;
+    padding-top: 100px;
+  }
+  .logo {
+    font-family: 'Slackey';
+    font-size: 60px;
+    line-height: 60px;
+    margin: 0;
+    text-align: center;
+    color: rgb(70, 120, 20);
+    text-shadow: 1px 1px 2px rgb(255, 255, 0), -1px -1px 2px rgb(255, 255, 0),
+      5px 8px 8px rgb(0, 0, 0);
+  }
+  .continue-button {
+    appearance: none;
+    border: 0;
+    background: inherit;
+    font-family: inherit;
+    color: inherit;
+    cursor: pointer;
+    font-size: 30px;
+  }
+</style>
